bf0b0c06ef75424dfe8341afdf86dae0bbf2d247
Nitesh Bodekar <niteshbodekar16@gmail.com>
2019-10-03 10:08:53 +0530

    first commit

---
+++ src/main/java/com/java/springBoot/hello/HelloController.java
@@ -0,0 +1,19 @@
+package com.java.springBoot.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController		
+public class HelloController {
+	
+@RequestMapping("/hello")
+	public String sayHi() {
+		
+		
+		
+		return "Hi";
+	}
+
+	
+	
+}
